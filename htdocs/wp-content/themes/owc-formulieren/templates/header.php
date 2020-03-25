215f8d91f4e00f4875a410b460c354df271a1923
YvetteNikolov <yvettenikolov@gmail.com>
2020-03-25 13:45:59 +0100

    (feat): add custom checkboxes and radiobuttons

--- htdocs/wp-content/themes/owc-formulieren/templates/header.php
+++ htdocs/wp-content/themes/owc-formulieren/templates/header.php
@@ -11,6 +11,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="profile" href="http://gmpg.org/xfn/11">
+    <script src="https://kit.fontawesome.com/bf2d22097a.js" crossorigin="anonymous"></script>
     <?php wp_head(); ?>
 </head>
 
