3c9fa3eb758536c28bb9eaeb64c0d2c15ceb9609
YvetteNikolov <yvettenikolov@gmail.com>
2020-03-25 14:10:17 +0100

    (feat): add filter to change header logo url

--- htdocs/wp-content/themes/hoekschewaard/functions.php
+++ htdocs/wp-content/themes/hoekschewaard/functions.php
@@ -26,6 +26,40 @@ foreach ($includes as $file) {
 }
 unset($file, $filepath);
 
-// GravityForms
+/**
+ * Remove Gravity Forms styling
+ */
 add_filter('pre_option_rg_gforms_disable_css', '__return_true');
 add_filter('pre_option_rg_gforms_enable_html5', '__return_true');
+
+/**
+ * Change the custom logo URL
+ */
+add_filter('get_custom_logo', function () {
+    $custom_logo_id = get_theme_mod('custom_logo'); // The logo
+    $site_url = 'gemeentehw.nl';
+
+    if ($custom_logo_id) {
+        // Attr
+        $custom_logo_attr = array(
+            'class'    => 'custom-logo',
+            'itemprop' => 'logo',
+        );
+
+        // Image alt
+        $image_alt = get_post_meta($custom_logo_id, '_wp_attachment_image_alt', true);
+        if (empty($image_alt)) {
+            $custom_logo_attr['alt'] = get_bloginfo('name', 'display');
+        }
+
+        // Get the image
+        $html = sprintf(
+            '<a href="%1$s" class="custom-logo-link" rel="home" itemprop="url">%2$s</a>',
+            esc_url($site_url),
+            wp_get_attachment_image($custom_logo_id, 'full', false, $custom_logo_attr)
+        );
+    }
+
+    // Return
+    return $html;
+});
