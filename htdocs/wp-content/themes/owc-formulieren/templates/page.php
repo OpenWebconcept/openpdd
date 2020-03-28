2603582c2ac3348c750cbda6a421e3b681592df2
YvetteNikolov <yvettenikolov@gmail.com>
2020-03-28 15:12:00 +0100

    (feat): add readspeaker implementation

--- htdocs/wp-content/themes/owc-formulieren/templates/page.php
+++ htdocs/wp-content/themes/owc-formulieren/templates/page.php
@@ -14,7 +14,16 @@ get_header();
 
             <div class="row">
                 <div class="col-md-12">
-                    <?php the_content(); ?>
+                    <div id="readspeaker_button1" class="rs_skip rsbtn rs_preserve mb-3">
+                        <a rel="nofollow" class="rsbtn_play" accesskey="L" title="Laat de tekst voorlezen met ReadSpeaker webReader" 
+                        href="//app-eu.readspeaker.com/cgi-bin/rsent?customerid=8150&amp;lang=nl_nl&amp;readid=readspeaker&amp;url=<?php echo get_permalink() ?>" >
+                            <span class="rsbtn_left rsimg rspart"><span class="rsbtn_text"><span>Lees voor</span></span></span>
+                            <span class="rsbtn_right rsimg rsplay rspart"></span>
+                        </a>
+                    </div>
+                    <div id="readspeaker">
+                        <?php the_content(); ?>
+                    </div>
                 </div>
             </div>
 
