d3301dc52f0b048c0fe4a9da2110814e0e145227
Robert Bossaert <robertb@yard.nl>
2021-04-22 11:18:17 +0000

    (chore): update fontawesome kit id
    Approved-by: Edwin Siebel
    Approved-by: Daniel

--- htdocs/wp-content/themes/owc-formulieren/templates/header.php
+++ htdocs/wp-content/themes/owc-formulieren/templates/header.php
@@ -11,7 +11,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="profile" href="http://gmpg.org/xfn/11">
-    <script src="https://kit.fontawesome.com/bf2d22097a.js" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/8442ade4bd.js" crossorigin="anonymous"></script>
     <script src="//cdn1.readspeaker.com/script/8150/webReader/webReader.js?pids=wr" type="text/javascript"></script>
     <?php wp_head(); ?>
 </head>
