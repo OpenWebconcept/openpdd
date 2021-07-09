186e8b47bf01c241430b226504fc1d1b39923a92
Edwin Siebel <edwin@edwinsiebel.nl>
2021-07-09 10:52:31 +0200

    (chore): install duplicate post

--- htdocs/wp-config.php
+++ htdocs/wp-config.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Autoload dependencies with Composer
  */
@@ -18,10 +19,10 @@ define('DB_PASSWORD', getenv('DB_PASSWORD'));
 define('DB_HOST', getenv('DB_HOST'));
 
 
-define('WP_DEBUG', getenv('WP_DEBUG'));
-define('WP_DEBUG_DISPLAY', getenv('WP_DEBUG_DISPLAY'));
-define('WP_DEBUG_LOG', getenv('WP_DEBUG_LOG'));
-define('SCRIPT_DEBUG', getenv('SCRIPT_DEBUG'));
+define('WP_DEBUG', filter_var(getenv('WP_DEBUG'), FILTER_SANITIZE_STRING));
+define('WP_DEBUG_DISPLAY', filter_var(getenv('WP_DEBUG_DISPLAY'), FILTER_SANITIZE_STRING));
+define('WP_DEBUG_LOG', filter_var(getenv('WP_DEBUG_LOG'), FILTER_SANITIZE_STRING));
+define('SCRIPT_DEBUG', filter_var(getenv('SCRIPT_DEBUG'), FILTER_SANITIZE_STRING));
 
 /**
  * Setup secret keys.
@@ -42,8 +43,24 @@ define('DB_CHARSET', 'utf8');
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
 
-/** License key of WP Migrate DB Pro */
+define('WP_ALLOW_MULTISITE', true);
+define('MULTISITE', true);
+define('SUBDOMAIN_INSTALL', false);
+define('DOMAIN_CURRENT_SITE', getenv('DOMAIN_CURRENT_SITE'));
+define('PATH_CURRENT_SITE', '/');
+define('SITE_ID_CURRENT_SITE', 1);
+define('BLOG_ID_CURRENT_SITE', 1);
+
+/**
+ * TWOFAS
+ */
+define('TWOFAS_LIGHT_CHECK_CONFLICTED_PLUGINS', false);
+
+/**
+ * License keys
+ */
 define('WPMDB_LICENCE', getenv('WPMDB_LICENCE'));
+define('META_BOX_KEY', getenv('META_BOX_KEY'));
 
 /**
  * WordPress Database Table prefix.
@@ -62,22 +79,12 @@ define('WP_CONTENT_DIR', dirname(__FILE__) . '/wp-content');
  * If call is made via cli, ignore global $_SERVER variables
  */
 if ('cli' !== php_sapi_name()) {
-    if ((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') || $_SERVER['SERVER_PORT'] == 443) {
+    if ((!empty($_SERVER['HTTPS']) && 'off' !== $_SERVER['HTTPS']) || 443 == $_SERVER['SERVER_PORT']) {
         define('WP_CONTENT_URL', 'https://' . $_SERVER['HTTP_HOST'] . '/wp-content');
     } else {
         define('WP_CONTENT_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/wp-content');
     }
 }
-
-define('WP_ALLOW_MULTISITE', true);
-define('MULTISITE', true);
-define('SUBDOMAIN_INSTALL', false);
-define('DOMAIN_CURRENT_SITE', getenv('DOMAIN_CURRENT_SITE'));
-define('PATH_CURRENT_SITE', '/');
-define('SITE_ID_CURRENT_SITE', 1);
-define('BLOG_ID_CURRENT_SITE', 1);
-
-define('TWOFAS_LIGHT_CHECK_CONFLICTED_PLUGINS', false);
 
 /* That's all, stop editing! Happy blogging. */
 
