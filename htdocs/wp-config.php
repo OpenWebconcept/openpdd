90465656b7717060a8c373a523d5bc51c8707bf5
Simon <simon@yard.nl>
2021-11-08 10:52:14 +0100

    (fix): phpdotenv incompatibility

--- htdocs/wp-config.php
+++ htdocs/wp-config.php
@@ -13,28 +13,28 @@ require __DIR__.'/../vendor/autoload.php';
 /**
  * Setup Database Configuration
  */
-define('DB_NAME', getenv('DB_DATABASE'));
-define('DB_USER', getenv('DB_USER'));
-define('DB_PASSWORD', getenv('DB_PASSWORD'));
-define('DB_HOST', getenv('DB_HOST'));
+define('DB_NAME', env('DB_DATABASE'));
+define('DB_USER', env('DB_USER'));
+define('DB_PASSWORD', env('DB_PASSWORD'));
+define('DB_HOST', env('DB_HOST'));
 
-define('WP_DEBUG', filter_var(getenv('WP_DEBUG'), FILTER_SANITIZE_STRING));
-define('WP_DEBUG_DISPLAY', filter_var(getenv('WP_DEBUG_DISPLAY'), FILTER_SANITIZE_STRING));
-define('WP_DEBUG_LOG', filter_var(getenv('WP_DEBUG_LOG'), FILTER_SANITIZE_STRING));
-define('SCRIPT_DEBUG', filter_var(getenv('SCRIPT_DEBUG'), FILTER_SANITIZE_STRING));
+define('WP_DEBUG', filter_var(env('WP_DEBUG'), FILTER_SANITIZE_STRING));
+define('WP_DEBUG_DISPLAY', filter_var(env('WP_DEBUG_DISPLAY'), FILTER_SANITIZE_STRING));
+define('WP_DEBUG_LOG', filter_var(env('WP_DEBUG_LOG'), FILTER_SANITIZE_STRING));
+define('SCRIPT_DEBUG', filter_var(env('SCRIPT_DEBUG'), FILTER_SANITIZE_STRING));
 
 /**
  * Setup secret keys.
  */
-define('AUTH_KEY', getenv('AUTH_KEY'));
-define('SECURE_AUTH_KEY', getenv('SECURE_AUTH_KEY'));
-define('LOGGED_IN_KEY', getenv('LOGGED_IN_KEY'));
-define('NONCE_KEY', getenv('NONCE_KEY'));
-define('AUTH_SALT', getenv('AUTH_SALT'));
-define('SECURE_AUTH_SALT', getenv('SECURE_AUTH_SALT'));
-define('LOGGED_IN_SALT', getenv('LOGGED_IN_SALT'));
-define('NONCE_SALT', getenv('NONCE_SALT'));
-define('WP_CACHE_KEY_SALT', getenv('WP_CACHE_KEY_SALT'));
+define('AUTH_KEY', env('AUTH_KEY'));
+define('SECURE_AUTH_KEY', env('SECURE_AUTH_KEY'));
+define('LOGGED_IN_KEY', env('LOGGED_IN_KEY'));
+define('NONCE_KEY', env('NONCE_KEY'));
+define('AUTH_SALT', env('AUTH_SALT'));
+define('SECURE_AUTH_SALT', env('SECURE_AUTH_SALT'));
+define('LOGGED_IN_SALT', env('LOGGED_IN_SALT'));
+define('NONCE_SALT', env('NONCE_SALT'));
+define('WP_CACHE_KEY_SALT', env('WP_CACHE_KEY_SALT'));
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
@@ -45,7 +45,7 @@ define('DB_COLLATE', '');
 define('WP_ALLOW_MULTISITE', true);
 define('MULTISITE', true);
 define('SUBDOMAIN_INSTALL', false);
-define('DOMAIN_CURRENT_SITE', getenv('DOMAIN_CURRENT_SITE'));
+define('DOMAIN_CURRENT_SITE', env('DOMAIN_CURRENT_SITE'));
 define('PATH_CURRENT_SITE', '/');
 define('SITE_ID_CURRENT_SITE', 1);
 define('BLOG_ID_CURRENT_SITE', 1);
@@ -58,8 +58,8 @@ define('TWOFAS_LIGHT_CHECK_CONFLICTED_PLUGINS', false);
 /**
  * License keys
  */
-define('WPMDB_LICENCE', getenv('WPMDB_LICENCE'));
-define('META_BOX_KEY', getenv('META_BOX_KEY'));
+define('WPMDB_LICENCE', env('WPMDB_LICENCE'));
+define('META_BOX_KEY', env('META_BOX_KEY'));
 
 /**
  * WordPress Database Table prefix.
