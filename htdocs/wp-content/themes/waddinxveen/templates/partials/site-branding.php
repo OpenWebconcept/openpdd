4cc2f9d3bd7720afd6b8b10c1bc6dc1d515339ef
YvetteNikolov <yvettenikolov@gmail.com>
2022-11-10 15:24:36 +0100

    (style): waddinxveen child theme

--- htdocs/wp-content/themes/waddinxveen/templates/partials/site-branding.php
+++
@@ -1,9 +0,0 @@
-<?php declare(strict_types=1);
-$name = get_bloginfo('name');
-$description = get_bloginfo('description', 'display');
-?>
-
-<div class="site-branding | d-flex align-items-center mr-sm-4">
-    <img class="custom-logo"
-        src="<?php echo sprintf('%s/assets/img/veilig-thuis-logo.png', get_stylesheet_directory_uri()); ?>" />
-</div>
