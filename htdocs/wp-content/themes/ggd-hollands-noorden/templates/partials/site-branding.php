eb6b25fc6d80c615fbf789a2e6d5c2e32f680236
YvetteNikolov <yvettenikolov@gmail.com>
2023-03-20 14:49:26 +0100

    (a11y): add empty alt text to site-branding of ggdhn

--- htdocs/wp-content/themes/ggd-hollands-noorden/templates/partials/site-branding.php
+++ htdocs/wp-content/themes/ggd-hollands-noorden/templates/partials/site-branding.php
@@ -4,6 +4,6 @@ $description = get_bloginfo('description', 'display');
 ?>
 
 <div class="site-branding | d-flex align-items-center mr-sm-4">
-    <img class="custom-logo"
+    <img class="custom-logo" alt=""
         src="<?php echo sprintf('%s/assets/img/veilig-thuis-logo.png', get_stylesheet_directory_uri()); ?>" />
 </div>
