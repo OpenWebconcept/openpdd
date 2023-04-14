801c9b88e9d841b4b0b183d4c3e8ba2a523b6a70
Robert Bossaert <contact@robertbossaert.com>
2023-04-14 15:07:57 +0200

    (feat): place readspeaker button below title

--- htdocs/wp-content/themes/owc-formulieren/templates/page.php
+++ htdocs/wp-content/themes/owc-formulieren/templates/page.php
@@ -4,7 +4,7 @@ get_header();
 <main class="page-main">
     <div class="container | bg-white p-3 p-md-4 p-lg-5 my-5 shadow">
         <?php while (have_posts()) : the_post(); ?>
-        <header class="section__header | d-flex align-items-center justify-content-between pb-3">
+        <header class="section__header | d-flex flex-column pb-3">
             <h1 class="section__title">
                 <?php echo get_the_title() ?>
             </h1>
