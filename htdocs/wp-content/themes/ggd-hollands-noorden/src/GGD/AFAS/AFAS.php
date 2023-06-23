49dbcbc5e8241a2c86e4f2ed0519179f8d3f6fcc
Mike van den Hoek <mike@yard.nl>
2023-06-23 15:13:50 +0200

    (sc27305): AFAS connection supports 2 forms instead of 1

--- htdocs/wp-content/themes/ggd-hollands-noorden/src/GGD/AFAS/AFAS.php
+++ htdocs/wp-content/themes/ggd-hollands-noorden/src/GGD/AFAS/AFAS.php
@@ -8,15 +8,17 @@ use GGD\AFAS\Models\Person;
 
 class AFAS
 {
+    protected TeamsLogger $logger;
+    
     public function __construct()
     {
-        $this->teams = $this->resolveTeams();
+        $this->logger = $this->resolveTeams();
     }
 
     public function resolveTeams(): TeamsLogger
     {
         try {
-            if (!function_exists('GGD\AFAS\Helpers\resolve')) {
+            if (! function_exists('GGD\AFAS\Helpers\resolve')) {
                 throw new \Exception;
             }
 
@@ -28,30 +30,25 @@ class AFAS
 
     public function afterFormSubmission(array $entry, array $form): array
     {
-        if (strtolower($form['title']) !== 'klachtenformulier') {
+        /**
+         * Could done better, client chose the cheaper option.
+         * If there are going to be more forms that should have a connection with AFAS this code needs to be rewritten.
+         * Something with fetching the entries dynamically instead of harde coded IDs.
+         */
+        if (strtolower($form['cssClass']) === 'klachtenformulier') {
+            $formData = $this->getFormDataDefaultForm($entry);
+        } elseif(strtolower($form['cssClass']) === 'klachtenformulier-vt-nhn') {
+            $formData = $this->getFormDataFormVtNHN($entry);
+        } else {
             return $form;
         }
-
-        $formData = [
-            'firstName' => rgar($entry, '11.3'),
-            'lastNamePrefix' => rgar($entry, '11.4'),
-            'lastName' => rgar($entry, '11.6'),
-            'gender' => rgar($entry, '3'),
-            'street' => rgar($entry, '4.1'),
-            'housenumber' => rgar($entry, '4.2'),
-            'city' => rgar($entry, '4.3'),
-            'postalCode' => rgar($entry, '4.5'),
-            'email' => rgar($entry, '5'),
-            'phonenumber' => rgar($entry, '6'),
-            'complaint' => rgar($entry, '8'),
-        ];
 
         $formData = new FormData($formData);
 
         try {
             $person = $this->getPerson($formData);
         } catch (\Exception $e) {
-            $this->teams->addRecord('error', 'GGD klachtenformulier', [
+            $this->logger->addRecord('error', 'GGD klachtenformulier', [
                 'message' => $e->getMessage()
             ]);
             $person = [];
@@ -60,7 +57,7 @@ class AFAS
         try {
             $person = $this->handleNewPerson($formData, $person);
         } catch (\Exception $e) {
-            $this->teams->addRecord('error', 'GGD klachtenformulier', [
+            $this->logger->addRecord('error', 'GGD klachtenformulier', [
                 'message' => $e->getMessage()
             ]);
             return $form;
@@ -73,12 +70,52 @@ class AFAS
         $subject = $this->createSubject(Person::makeFrom($person), $formData);
 
         if (empty($subject)) {
-            $this->teams->addRecord('error', 'GGD klachtenformulier', [
+            $this->logger->addRecord('error', 'GGD klachtenformulier', [
                 'message' => sprintf('AFAS error: could not create a subject in AFAS for person (%s).', $formData->email())
             ]);
         }
 
         return $form;
+    }
+
+    /**
+     * Create formdata based on form with ID 1.
+     */
+    protected function getFormDataDefaultForm(array $entry): array
+    {
+        return [
+            'firstName' => rgar($entry, '11.3'),
+            'lastNamePrefix' => rgar($entry, '11.4'),
+            'lastName' => rgar($entry, '11.6'),
+            'gender' => rgar($entry, '3'),
+            'street' => rgar($entry, '4.1'),
+            'housenumber' => rgar($entry, '4.2'),
+            'city' => rgar($entry, '4.3'),
+            'postalCode' => rgar($entry, '4.5'),
+            'email' => rgar($entry, '5'),
+            'phonenumber' => rgar($entry, '6'),
+            'complaint' => rgar($entry, '8'),
+        ];
+    }
+
+    /**
+     * Create formdata based on form with ID 10.
+     */
+    protected function getFormDataFormVtNHN(array $entry): array
+    {
+        return [
+            'firstName' => rgar($entry, '1'),
+            'lastNamePrefix' => rgar($entry, '7'),
+            'lastName' => rgar($entry, '2'),
+            'gender' => rgar($entry, '3'),
+            'street' => rgar($entry, '4.1'),
+            'housenumber' => rgar($entry, '4.2'),
+            'city' => rgar($entry, '4.3'),
+            'postalCode' => rgar($entry, '4.5'),
+            'email' => rgar($entry, '5'),
+            'phonenumber' => rgar($entry, '6'),
+            'complaint' => rgar($entry, '8'),
+        ];
     }
 
     /**
@@ -122,7 +159,7 @@ class AFAS
         try {
             $subject = (new \GGD\AFAS\SOAP\Requests\createSubject(resolve('SOAPPostRequest'), $person, $formData))->create();
         } catch (\Exception $e) {
-            $this->teams->addRecord('error', 'GGD klachtenformulier', [
+            $this->logger->addRecord('error', 'GGD klachtenformulier', [
                 'message' => sprintf('AFAS error: could not create a subject on behalf of person (%s). Error: %s', $formData->email(), $e->getMessage())
             ]);
             return '';
