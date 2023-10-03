a592968cb13a218c6a464d2ed647993413674f84
YvetteNikolov <yvettenikolov@gmail.com>
2023-10-03 10:58:18 +0200

    (feat): add full page editing template & scss

---
+++ htdocs/wp-content/themes/owc-formulieren/templates/template-fpe.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Template name: Full page editing
+ */
+
+get_header();
+
+?>
+
+<main class="page-main" id="readspeaker">
+    <?php the_content(); ?>
+</main>
+
+<?php get_footer();
