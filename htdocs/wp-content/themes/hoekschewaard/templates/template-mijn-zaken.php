9a39f6c288588ed5dc892f4e6f59fcc37be077d4
YvetteNikolov <yvettenikolov@gmail.com>
2023-10-04 10:52:55 +0200

    (feat): first setup of hw navbar and footer

---
+++ htdocs/wp-content/themes/hoekschewaard/templates/template-mijn-zaken.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+/**
+ * Template name: Mijn Zaken
+ */
+get_template_part('templates/mijn-zaken/header');
+
+?>
+
+<main class="page-main" id="readspeaker">
+    <?php the_content(); ?>
+</main>
+
+<?php get_template_part('templates/mijn-zaken/footer');
