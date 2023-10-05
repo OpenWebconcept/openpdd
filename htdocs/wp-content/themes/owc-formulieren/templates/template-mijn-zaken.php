51839fac82320a6199193de02008fc50d22cc00d
Paul van Impelen <paul@acato.nl>
2023-10-05 10:41:53 +0200

    Add new template for "Mijn Zaken"

--- htdocs/wp-content/themes/owc-formulieren/templates/template-mijn-zaken.php
+++ htdocs/wp-content/themes/owc-formulieren/templates/template-mijn-zaken.php
@@ -2,11 +2,16 @@
 /**
  * Template name: Mijn Zaken
  */
+
 get_template_part('mijn-zaken/header');
 ?>
-
-<main class="page-main" id="readspeaker">
-    <?php the_content(); ?>
-</main>
-
-<?php get_template_part('mijn-zaken/footer');
+    <main class="page-main page-main--mijn-zaken container" id="readspeaker">
+        <aside class="page-main__aside">
+            <?php get_template_part('partials/sidebar', 'mijn-zaken'); ?>
+        </aside>
+        <article class="page-main__content">
+            <?php the_content(); ?>
+        </article>
+    </main>
+<?php
+get_template_part('templates/mijn-zaken/footer');
