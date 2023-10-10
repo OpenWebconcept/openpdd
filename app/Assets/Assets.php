ee93cb3fffdf00a616ec31849480b80b42e298ce
YvetteNikolov <yvettenikolov@gmail.com>
2023-10-10 11:33:42 +0200

    (feat): fix and centralize enqueueing of assets

---
+++ app/Assets/Assets.php
@@ -0,0 +1,65 @@
+<?php declare(strict_types=1);
+
+namespace App\Assets;
+
+class Assets
+{
+    /**
+     * Enqueues scripts and styles for the frontend.
+     */
+    public static function enqueueScripts()
+    {
+        $assetDetails = self::getAssetDetails('frontend.asset.php');
+
+        wp_register_script(
+            'theme',
+            $assetDetails['baseUrl'] . 'frontend.js',
+            $assetDetails['scriptAsset']['dependencies'],
+            $assetDetails['scriptAsset']['version'],
+        );
+
+        wp_enqueue_script('theme');
+        
+        wp_enqueue_style('theme', $assetDetails['baseUrl'] . 'frontend.css', [], $assetDetails['scriptAsset']['version']);
+    }
+
+    /**
+     * Enqueues scripts for the block editor.
+     */
+    public static function enqueueBlockEditorScripts()
+    {
+        $assetDetails = self::getAssetDetails('editor.asset.php');
+
+        wp_register_script(
+            'theme-block-editor',
+            $assetDetails['baseUrl'] . 'editor.js',
+            $assetDetails['scriptAsset']['dependencies'],
+            $assetDetails['scriptAsset']['version'],
+            true
+        );
+        
+        wp_enqueue_script('theme-block-editor');
+        wp_enqueue_style('theme-block-editor', $assetDetails['baseUrl'] . 'editor.css', [], $assetDetails['scriptAsset']['version']);
+    }
+
+    /**
+     * Retrieve common asset details.
+     *
+     * @param string $assetFileName The name of the php asset file.
+     */
+    private static function getAssetDetails($assetFileName): array
+    {
+        $baseUrl = get_stylesheet_directory_uri() . '/assets/dist/';
+        $basePath = get_stylesheet_directory() . '/assets/dist/';
+
+        $scriptAssetPath = $basePath . $assetFileName;
+        $scriptAsset = file_exists($scriptAssetPath) ? require($scriptAssetPath) : ['dependencies' => [], 'version' => round(microtime(true))];
+
+        return [
+            'baseUrl' => $baseUrl,
+            'basePath' => $basePath,
+            'scriptAssetPath' => $scriptAssetPath,
+            'scriptAsset' => $scriptAsset,
+        ];
+    }
+}
--- htdocs/wp-content/themes/baralbrandswaard/src/hooks.php
+++ htdocs/wp-content/themes/baralbrandswaard/src/hooks.php
@@ -96,15 +96,8 @@ add_filter('template', function ($stylesheet) {
     return dirname($stylesheet);
 });
 
-add_action('wp_enqueue_scripts', function () {
-    wp_enqueue_style('child-theme-style', get_stylesheet_directory_uri() . '/assets/dist/frontend.css', [], filemtime(__DIR__));
-    wp_enqueue_script('child-theme-script', get_template_directory_uri() . '/assets/dist/frontend.js', ['jquery'], filemtime(__DIR__), true);
-});
-
-add_action('enqueue_block_editor_assets', function () {
-    wp_enqueue_script('theme-blocks-js', get_stylesheet_directory_uri() . '/assets/dist/editor.js', [], filemtime(__DIR__));
-    wp_enqueue_style('theme-blocks-css', get_stylesheet_directory_uri() . '/assets/dist/editor.css', [], filemtime(__DIR__));
-});
+add_action('wp_enqueue_scripts', 'App\Assets\Assets::enqueueScripts');
+add_action('enqueue_block_editor_assets', 'App\Assets\Assets::enqueueBlockEditorScripts');
 
 /**
  * Remove Gravity Forms styling
