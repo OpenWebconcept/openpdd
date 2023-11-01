e2cc0501be23e3124dcfd613555c07be818d118c
Mike van den Hoek <mike@yard.nl>
2023-11-01 09:24:38 +0100

    (feat): add template single zaak

---
+++ htdocs/wp-content/themes/buren/templates/template-single-zaak.php
@@ -0,0 +1,52 @@
+<?php declare(strict_types=1);
+
+$zaak = get_query_var('zaak');
+
+/**
+ * Template name: Mijn Zaak
+ */
+get_template_part('mijn-zaken/header');
+
+?>
+    <main class="page-main page-main--mijn-zaken container" id="readspeaker">
+        <aside class="page-main__aside">
+            <?php get_template_part('templates/mijn-zaken/sidebar'); ?>
+        </aside>
+        <article class="page-main__content">
+			<?php if ($zaak) : ?>
+			<div class="zaak-header">
+				<h1 class="zaak-header-title">Zaak: <?php echo $zaak->title(); ?></h1>
+			</div>
+			<div class="zaak-details">
+				<h2>Details</h2>
+				<table class="zaak-details-table">
+					<tr>
+						<th>Registratiedatum</th>
+						<td><?php echo $zaak->registratiedatum->format('j F Y'); ?> </td>
+						<td><a href="#">Bekijk originele aanvraag</a></td>
+					</tr>
+					<tr>
+						<th>Startdatum</th>
+						<td><?php echo $zaak->startdatum->format('j F Y'); ?></td>
+					</tr>
+					<tr>
+						<th>Zaaknummer</th>
+						<td><?php echo $zaak->identificatie; ?></td>
+					</tr>
+					<tr>
+						<th>Status</th>
+						<td><?php echo $status; ?></td>
+					</tr>
+				</table>
+			</div>
+			<?php else : ?>
+				<div class="zaak-header">
+					<h1 class="zaak-header-title">Er ging iets fout..</h1>
+				</div>
+				<p>Uw zaak kon niet gevonden worden.</p>
+			<?php endif ?>
+        </article>
+    </main>
+<?php
+
+get_template_part('mijn-zaken/footer');
