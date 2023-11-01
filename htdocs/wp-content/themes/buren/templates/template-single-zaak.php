f6e2ba41fa85fb76a2a8a9bc66909320e41fd977
YvetteNikolov <yvettenikolov@gmail.com>
2023-11-01 20:01:57 +0100

    (chore): add status and steps to the single zaak template

--- htdocs/wp-content/themes/buren/templates/template-single-zaak.php
+++ htdocs/wp-content/themes/buren/templates/template-single-zaak.php
@@ -3,7 +3,7 @@
 $zaak = get_query_var('zaak');
 
 /**
- * Template name: Mijn Zaak
+ * Template name: Mijn Zaken Single
  */
 get_template_part('mijn-zaken/header');
 
@@ -38,6 +38,48 @@ get_template_part('mijn-zaken/header');
 						<td><?php echo $zaak->statusExplanation() ?: 'Onbekend'; ?></td>
 					</tr>
 				</table>
+			</div>
+			<div class="zaak-process">
+				<h2>Status</h2>
+				<?php if (empty($zaak->steps()) || $zaak->hasNoStatus()) : ?>
+					<p>Momenteel is er geen status beschikbaar.</p>
+				<?php else : ?>
+					<ol class="zaak-process-steps">
+						<?php foreach ($zaak->steps() as $step) : ?>
+							<?php
+                            if (! empty($zaak->statusHistory())) {
+                                $statusUpdate = $zaak->statusHistory()->filter(function ($status) use ($step) {
+                                    return $status->statustype->url === $step->url;
+                                })->first();
+                            }
+                            $isPastIcon = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M435.848 83.466L172.804 346.51l-96.652-96.652c-4.686-4.686-12.284-4.686-16.971 0l-28.284 28.284c-4.686 4.686-4.686 12.284 0 16.971l133.421 133.421c4.686 4.686 12.284 4.686 16.971 0l299.813-299.813c4.686-4.686 4.686-12.284 0-16.971l-28.284-28.284c-4.686-4.686-12.284-4.686-16.97 0z"/></svg>';
+						    ?>
+
+							<li class="zaak-process-steps__step <?= $step->isEndStatus() ? 'zaak-process-steps__step--current' : ''; ?><?= ! $step->isEndStatus() ? 'zaak-process-steps__step--past' : ''; ?>" aria-current="">
+								<span class="zaak-process-steps__step-marker">
+									<?= ! $step->isEndStatus() ? $isPastIcon : $step->volgnummer; ?>
+								</span>
+								<span class="zaak-process-steps__step-heading-label">
+									<?php echo $step->omschrijving ?>
+									<?php if ($statusUpdate) : ?>
+										<small>(<?= $statusUpdate->datumStatusGezet->format('d-m-Y'); ?>)</small>
+									<?php endif; ?>
+								</span>
+								<?php if (false) : ?>
+									<ol class="zaak-process-steps__substeps">
+										<li class="zaak-process-steps__substep">
+											<div class="zaak-process-steps__substep-marker">
+											</div>
+											<div class="zaak-process-steps__substep-heading-label">
+												<?php echo $substep->data['omschrijving'] ?>
+											</div>
+										</li>
+									</ol>
+								<?php endif; ?>
+							</li>
+						<?php endforeach; ?>
+					<?php endif; ?>
+				</ol>
 			</div>
 			<?php else : ?>
 				<div class="zaak-header">
