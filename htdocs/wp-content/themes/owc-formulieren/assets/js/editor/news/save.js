78c89c76e54a0f769811cc65278a278aa49de8ff
YvetteNikolov <yvettenikolov@gmail.com>
2023-11-20 10:15:20 +0100

    (chore): temporarily remove unused blocks to merge to master

--- htdocs/wp-content/themes/owc-formulieren/assets/js/editor/news/save.js
+++
@@ -1,9 +0,0 @@
-const Save = (props) => {
-
-    // This data will be fetched from the OpenPUB API. The function for this can be found in /assets/js/frontend/components/fetch-openpub-data.js.
-    return (
-      <ul className="openpdd-news"></ul>
-    );
-};
-
-export default Save;
