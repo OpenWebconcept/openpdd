4c7382cc2ab1c84dcc3b0d3897f1f0dea361c8b4
Simon <simon@yard.nl>
2023-11-21 14:58:18 +0100

    wip

--- htdocs/wp-content/themes/buren/src/hooks.php
+++ htdocs/wp-content/themes/buren/src/hooks.php
@@ -14,7 +14,7 @@ add_action('init', function () {
         return;
     }
 
-    if (!function_exists('csp_nonce')) {
+    if (! function_exists('csp_nonce')) {
         return;
     }
 
@@ -32,13 +32,13 @@ add_action('init', function () {
 /**
  * Add a complete CSP to the document.
  */
-add_action('send_headers', function () {
-    if (is_admin()) {
-        return;
-    }
-    
-    \Bepsvpt\SecureHeaders\SecureHeaders::fromFile(APP_ROOT . '/config/secure-headers.php')->send();
-});
+// add_action('send_headers', function () {
+//     if (is_admin()) {
+//         return;
+//     }
+
+//     \Bepsvpt\SecureHeaders\SecureHeaders::fromFile(APP_ROOT . '/config/secure-headers.php')->send();
+// });
 
 function is_rest()
 {
@@ -57,6 +57,7 @@ function is_rest()
     // (#4)
     $rest_url = wp_parse_url(trailingslashit(rest_url()));
     $current_url = wp_parse_url(add_query_arg([ ]));
+
     return (strpos($current_url['path'], $rest_url['path'], 0) === 0);
 }
 
@@ -166,6 +167,7 @@ add_filter('lv_default_error_messages', function ($default_messages) {
  */
 add_filter('gform_incomplete_submissions_expiration_days', function ($expiration_days) {
     $expiration_days = 7;
+
     return $expiration_days;
 });
 
@@ -259,7 +261,7 @@ add_action('after_switch_theme', function () {
     $caps = array_merge($caps, $role->getGravityFormsCaps());
 
     foreach ($caps as $cap) {
-        if (!$role->getRole()->has_cap($cap)) {
+        if (! $role->getRole()->has_cap($cap)) {
             $role->addCap($cap);
         }
     }
@@ -294,7 +296,7 @@ add_action('wp_enqueue_scripts', function () {
  */
 function theme_script_loader_tag($tag, $handle)
 {
-    if (!is_admin()) {
+    if (! is_admin()) {
         $scripts_to_load = [
             [
                 ('name')      => 'jquery',
@@ -303,11 +305,11 @@ function theme_script_loader_tag($tag, $handle)
             [
                 ('name')      => 'jquery-ui-core',
                 ('integrity') => 'sha384-4D3G3GikQs6hLlLZGdz5wLFzuqE9v4yVGAcOH86y23JqBDPzj9viv0EqyfIa6YUL',
-            ]
+            ],
         ];
-    
+
         $key = array_search($handle, array_column($scripts_to_load, 'name'));
-        
+
         if (false !== $key) {
             $tag = str_replace('></script>', ' integrity=\'' . $scripts_to_load[$key]['integrity'] . '\' crossorigin=\'anonymous\'></script>', $tag);
         }
