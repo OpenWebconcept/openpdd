70395ee994c456be5f49bc80040c401a47c5be9f
Lara van Diemen <lara@yard.nl>
2023-12-04 11:45:19 +0100

    (chore): only show readspeaker for ggd and hoekschewaard

--- htdocs/wp-content/themes/owc-formulieren/templates/page.php
+++ htdocs/wp-content/themes/owc-formulieren/templates/page.php
@@ -8,15 +8,17 @@ get_header();
             <h1 class="page-title | mb-3">
                 <?php echo get_the_title() ?>
             </h1>
-            <div id="readspeaker_button1" class="rs_skip rsbtn rs_preserve mb-3">
-                <a rel="nofollow" class="rsbtn_play" accesskey="L"
-                    title="Laat de tekst voorlezen met ReadSpeaker webReader"
-                    href="//app-eu.readspeaker.com/cgi-bin/rsent?customerid=<?php echo get_current_blog_id() === env('GGD_SITE_ID', 5) ? '13499' : '8150'; ?>&amp;lang=nl_nl&amp;readid=readspeaker&amp;url=<?php echo get_permalink() ?>">
-                    <span class="rsbtn_left rsimg rspart"><span class="rsbtn_text"><span>Lees
-                                voor</span></span></span>
-                    <span class="rsbtn_right rsimg rsplay rspart"></span>
-                </a>
-            </div>
+			<?php if (get_current_blog_id() === env('GGD_SITE_ID', 5) || get_current_blog_id() === env('HW_SITE_ID', 4)): ?>
+				<div id="readspeaker_button1" class="rs_skip rsbtn rs_preserve mb-3">
+					<a rel="nofollow" class="rsbtn_play" accesskey="L"
+						title="Laat de tekst voorlezen met ReadSpeaker webReader"
+						href="//app-eu.readspeaker.com/cgi-bin/rsent?customerid=<?php echo get_current_blog_id() === env('GGD_SITE_ID', 5) ? '13499' : '8150'; ?>&amp;lang=nl_nl&amp;readid=readspeaker&amp;url=<?php echo get_permalink() ?>">
+						<span class="rsbtn_left rsimg rspart"><span class="rsbtn_text"><span>Lees
+									voor</span></span></span>
+						<span class="rsbtn_right rsimg rsplay rspart"></span>
+					</a>
+				</div>
+			<?php endif; ?>
         </header>
         <div class="page-content" id="readspeaker">
             <?php the_content(); ?>
