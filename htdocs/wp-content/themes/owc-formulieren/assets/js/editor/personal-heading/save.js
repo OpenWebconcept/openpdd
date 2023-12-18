2799bccbce56c9bc81361d0e2bc960396f2670f7
Gijs <gijsvanarem@acato.nl>
2023-12-18 14:43:51 +0100

    dynamic header is now dynamic, fillable through CMS

--- htdocs/wp-content/themes/owc-formulieren/assets/js/editor/personal-heading/save.js
+++ htdocs/wp-content/themes/owc-formulieren/assets/js/editor/personal-heading/save.js
@@ -1,24 +1,57 @@
-const Save = (props) => {
+import { __ } from '@wordpress/i18n';
+import { RichText } from '@wordpress/block-editor';
 
-    return (
-      <div className="openpdd-personal-heading">
-          <div className="openpdd-personal-heading__content">
-              <h1 className="openpdd-personal-heading__header">
-                  Goedemorgen Brigitta!
-              </h1>
-              <p>
-                  Alle voor u relevante informatie van de gemeente op 1 plek! Dat is MijnHW.
-              </p>
-          </div>
+const Save = ( { attributes } ) => {
 
-          <div className="openpdd-personal-heading__image-wrapper">
-              <img
-                className="openpdd-personal-heading__image"
-                src="https://via.placeholder.com/600x300"
-                alt="Placeholder image" />
-          </div>
-      </div>
-    );
+	return (
+		<div className="openpdd-personal-heading">
+			<div className="openpdd-personal-heading__content">
+				<h1 className="openpdd-personal-heading__header">
+					<span className="dynamic openpdd-personal-heading__morning">
+						{__( 'Goedemorgen', 'owc-formulieren' )}
+					</span>
+					<span className="dynamic openpdd-personal-heading__afternoon">
+						{__( 'Goedemiddag', 'owc-formulieren' )}
+					</span>
+					<span className="dynamic openpdd-personal-heading__evening">
+						{__( 'Goedenavond', 'owc-formulieren' )}
+					</span>
+					<span>
+						Brigitta!
+					</span>
+				</h1>
+
+				{attributes.textContent && (
+					<RichText.Content
+						value={attributes.textContent}
+					/>
+				)}
+			</div>
+
+			<div className="openpdd-personal-heading__image-wrapper">
+				{attributes.bgImageMorningUrl && (
+					<img
+						className="openpdd-personal-heading__image openpdd-personal-heading__image--morning"
+						src={attributes.bgImageMorningUrl}
+						alt="Placeholder image" />
+				)}
+
+				{attributes.bgImageAfternoonUrl && (
+					<img
+						className="openpdd-personal-heading__image openpdd-personal-heading__image--afternoon"
+						src={attributes.bgImageAfternoonUrl}
+						alt="Placeholder image" />
+				)}
+
+				{attributes.bgImageEveningUrl && (
+					<img
+						className="openpdd-personal-heading__image openpdd-personal-heading__image--evening"
+						src={attributes.bgImageEveningUrl}
+						alt="Placeholder image" />
+				)}
+			</div>
+		</div>
+	);
 };
 
 export default Save;
--- htdocs/wp-content/themes/owc-formulieren/assets/js/frontend/app.js
+++ htdocs/wp-content/themes/owc-formulieren/assets/js/frontend/app.js
@@ -2,11 +2,13 @@ import A11yToolbar from './components/A11yToolbar';
 import Dropdown from './components/Dropdown';
 import Navigation from './components/Navigation';
 import fetchOpenPubData from './components/fetch-openpub-data';
+import dynamicPersonalHeader from './components/dynamic-personal-header';
 
 const app = () => {
 	A11yToolbar();
 	Dropdown();
 	Navigation();
+	dynamicPersonalHeader();
 	fetchOpenPubData( '7101', '.openpdd-news' );
 };
 
