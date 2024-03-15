5a48e67ea4b3fff9b3d492b7853f60e0725aa9db
Robert Bossaert <contact@robertbossaert.com>
2024-03-15 10:59:25 +0100

    (feat): add eidas plugin (#43)

--- htdocs/wp-content/themes/ggd-hollands-noorden/functions.php
+++ htdocs/wp-content/themes/ggd-hollands-noorden/functions.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 defined('ABSPATH') || exit;
 
+
 // PSR autoloader
 spl_autoload_register(function ($className) {
     $baseDir = __DIR__ . '/src/';
@@ -18,11 +19,11 @@ spl_autoload_register(function ($className) {
 $includes = [
     'src/Role.php',
     'src/hooks.php',
-    'src/setup.php'
+    'src/setup.php',
 ];
 
 foreach ($includes as $file) {
-    if (!$filepath = locate_template($file)) {
+    if (! $filepath = locate_template($file)) {
         trigger_error(sprintf(__('Error locating %s for inclusion', 'ggd'), $file), E_USER_ERROR);
     }
 
