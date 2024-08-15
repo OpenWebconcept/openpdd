80b6162eefc5a49514ea506962a475be8bcfb0c6
Maarten Bruna <14947039+ictbeheer@users.noreply.github.com>
2024-08-15 16:30:12 +0200

    (fix) remove template hack

--- htdocs/wp-content/themes/buren/src/hooks.php
+++ htdocs/wp-content/themes/buren/src/hooks.php
@@ -76,25 +76,6 @@ add_action('phpmailer_init', function (\PHPMailer\PHPMailer\PHPMailer $phpmailer
     }
 });
 
-/**
- * Here's what's happening with these hooks:
- * 1. WordPress detects theme in themes/theme-name
- * 2. We tell WordPress that the theme is actually in themes/theme/path-to-configured-templates-directory
- * 3. When we call get_template_directory() or get_template_directory_uri(), we point it back to themes/theme
- *
- * We do this so that the Template Hierarchy will look in themes/theme/templates for core WordPress themes
- * But functions.php, style.css, and index.php are all still located in themes/theme
- *
- * get_template_directory()   -> /srv/www/example.com/current/web/app/themes/theme
- * get_stylesheet_directory() -> /srv/www/example.com/current/web/app/themes/theme
- * locate_template()
- * ├── STYLESHEETPATH         -> /srv/www/example.com/current/web/app/themes/theme
- * └── TEMPLATEPATH           -> /srv/www/example.com/current/web/app/themes/theme/templates
- */
-add_filter('template', function ($stylesheet) {
-    return dirname($stylesheet);
-});
-
 add_action('wp_enqueue_scripts', 'App\Assets\Assets::enqueueScripts');
 add_action('enqueue_block_editor_assets', 'App\Assets\Assets::enqueueBlockEditorScripts');
 
