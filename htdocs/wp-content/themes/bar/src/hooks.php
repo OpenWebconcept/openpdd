6eda9450cce250f77bd1e4cbfb63fe78bf7bd6ac
Maarten Bruna <14947039+ictbeheer@users.noreply.github.com>
2024-08-16 14:09:58 +0200

    (chore) normalize csp

--- htdocs/wp-content/themes/bar/src/hooks.php
+++ htdocs/wp-content/themes/bar/src/hooks.php
@@ -3,67 +3,6 @@
 declare(strict_types=1);
 
 /**
- * Add CSP to admin-ajax and initialize nonces for resources.
- */
-add_action('init', function () {
-    if (strpos(sanitize_text_field($_SERVER['REQUEST_URI']), '/wp-admin/admin-ajax.php') !== false) {
-        header("Content-Security-Policy: default-src 'none'; script-src 'self'; style-src 'self'; img-src 'self'; font-src 'self'; frame-ancestors 'self';");
-    }
-
-    if (is_admin() or is_rest()) {
-        return;
-    }
-
-    if (! function_exists('csp_nonce')) {
-        return;
-    }
-
-    $nonceScript = csp_nonce('script');
-    $nonceStyle = csp_nonce('style');
-
-    ob_end_clean();
-    ob_start();
-    add_action('shutdown', function () use ($nonceScript, $nonceStyle) {
-        $content = ob_get_clean();
-        echo App\Security\CSP::make($content, $nonceScript, $nonceStyle)->add();
-    }, 0);
-});
-
-/**
- * Add a complete CSP to the document.
- */
-add_action('send_headers', function () {
-    if (is_admin()) {
-        return;
-    }
-
-    \Bepsvpt\SecureHeaders\SecureHeaders::fromFile(APP_ROOT . '/config/secure-headers.php')->send();
-});
-function is_rest()
-{
-    $prefix = rest_get_url_prefix();
-    if (
-        defined('REST_REQUEST') && REST_REQUEST // (#1)
-        || isset($_GET['rest_route']) // (#2)
-        && strpos(trim($_GET['rest_route'], '\\/'), $prefix, 0) === 0
-    ) {
-        return true;
-    }
-    // (#3)
-    global $wp_rewrite;
-    if (null === $wp_rewrite) {
-        $wp_rewrite = new WP_Rewrite();
-    }
-
-    // (#4)
-    $rest_url = wp_parse_url(trailingslashit(rest_url()));
-    $current_url = wp_parse_url(add_query_arg([]));
-    $current_url_path = $current_url['path'] ?? '';
-
-    return (strpos($current_url_path, $rest_url['path'], 0) === 0);
-}
-
-/**
  * Remove Gravity Forms styling
  */
 add_filter('pre_option_rg_gforms_disable_css', '__return_true');
