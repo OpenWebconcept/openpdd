ee52a638a5257fd1bd889456084114edeb68df92
Maarten Bruna <14947039+ictbeheer@users.noreply.github.com>
2024-08-16 17:13:07 +0200

    (chore) normalize theme support

--- htdocs/wp-content/themes/baralbrandswaard/src/setup.php
+++ htdocs/wp-content/themes/baralbrandswaard/src/setup.php
@@ -8,35 +8,6 @@ declare(strict_types=1);
 add_action('after_setup_theme', function () {
     load_theme_textdomain('baralbrandswaard', get_stylesheet_directory() . '/languages/');
 
-    // Add default posts and comments RSS feed links to head.
-    add_theme_support('automatic-feed-links');
-
-    /*
-     * Let WordPress manage the document title.
-     * By adding theme support, we declare that this theme does not use a
-     * hard-coded <title> tag in the document head, and expect WordPress to
-     * provide it for us.
-     */
-    add_theme_support('title-tag');
-
-    /*
-     * Enable support for Post Thumbnails on posts and pages.
-     *
-     * @link http://codex.wordpress.org/Function_Reference/add_theme_support#Post_Thumbnails
-     */
-    add_theme_support('post-thumbnails');
-
-    /*
-     * Add site logo since 4.5
-     */
-    add_theme_support('custom-logo', [
-        'height'      => 100,
-        'width'       => 300,
-        'flex-height' => true,
-        'flex-width'  => true,
-        'header-text' => ['site-title', 'site-description'],
-    ]);
-
     // This theme uses wp_nav_menu() in one location.
     register_nav_menus([
         'primary' => __('Primary Menu', 'baralbrandswaard'),
