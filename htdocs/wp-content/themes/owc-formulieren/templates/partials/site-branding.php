5efe5601d7b128567cad4621f3d4b00c70ac4303
Maarten Bruna <14947039+ictbeheer@users.noreply.github.com>
2024-08-22 14:57:42 +0200

    (chore) normalize site logo

--- htdocs/wp-content/themes/owc-formulieren/templates/partials/site-branding.php
+++ htdocs/wp-content/themes/owc-formulieren/templates/partials/site-branding.php
@@ -1,9 +1,4 @@
-<?php declare(strict_types=1);
-$name = get_bloginfo('name');
-$description = get_bloginfo('description', 'display');
-?>
 
 <div class="site-branding | d-flex align-items-center mr-sm-4">
-    <?php if (has_custom_logo()) : echo get_custom_logo();
-    endif; ?>
+    <?php the_custom_logo();?>
 </div>
