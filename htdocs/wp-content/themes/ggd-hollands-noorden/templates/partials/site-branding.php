d5c64f30a417701a9d6ba3cc42a48e76ec8527f7
Dik Takken <dik.takken@yard.nl>
2024-09-20 14:25:32 +0200

    (fix): Fix showing incorrect logo in header

--- htdocs/wp-content/themes/ggd-hollands-noorden/templates/partials/site-branding.php
+++ htdocs/wp-content/themes/ggd-hollands-noorden/templates/partials/site-branding.php
@@ -1,9 +1,11 @@
 <?php declare(strict_types=1);
-$name = get_bloginfo('name');
-$description = get_bloginfo('description', 'display');
+$isTemplateVeiligThuis = $isTemplateVeiligThuis ?? false;
 ?>
 
 <div class="site-branding | d-flex align-items-center mr-sm-4">
-    <img class="custom-logo" alt=""
-        src="<?php echo sprintf('%s/assets/img/veilig-thuis-logo.png', get_stylesheet_directory_uri()); ?>" />
+	<?php if ($isTemplateVeiligThuis) : ?>
+		<img class="custom-logo" alt="" src="<?php echo sprintf('%s/assets/img/veilig-thuis-logo.png', get_stylesheet_directory_uri()); ?>" />
+	<?php else : ?>
+		<?php the_custom_logo(); ?>
+	<?php endif; ?>
 </div>
