49961a40e0820abf1d336d864fa7e61c7b9e3fe3
Mike van den Hoek <mike@yard.nl>
2024-10-23 14:14:23 +0200

    (temp): disable add filter validation of templates to fryskemarren theme

--- htdocs/wp-content/themes/defryskemarren/functions.php
+++ htdocs/wp-content/themes/defryskemarren/functions.php
@@ -4,12 +4,13 @@ declare(strict_types=1);
 
 defined('ABSPATH') || exit;
 
-add_filter('owc_gravityforms_zaaksysteem_templates_to_validate', function (array $templates) {
-    $templates[] = 'template-single-zaak';
-    $templates[] = 'template-mijn-zaken';
-    $templates[] = 'template-mijn-zaken-main';
+// Enable when PR is merged: https://github.com/OpenWebconcept/plugin-owc-gravityforms-zaaksysteem/pull/31
+// add_filter('owc_gravityforms_zaaksysteem_templates_to_validate', function (array $templates) {
+//     $templates[] = 'template-single-zaak';
+//     $templates[] = 'template-mijn-zaken';
+//     $templates[] = 'template-mijn-zaken-main';
 
-    return $templates;
-});
+//     return $templates;
+// });
 
 add_filter('gform_enable_legacy_markup', '__return_true');
