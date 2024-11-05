643d339a3ac099da208e249b9b43bf7762e2c78b
YvetteNikolov <yvettenikolov@gmail.com>
2024-11-05 13:22:48 +0100

    (feat): add wp-block-navigation.is-style-tiles with icons

--- app/Sidebar/Sidebar.php
+++ app/Sidebar/Sidebar.php
@@ -8,16 +8,65 @@ class Sidebar
     {
         add_action('acf/include_fields', [$this, 'addAcfFields']);
         add_action('wp_nav_menu_objects', [$this, 'addIconsToMenuItems'], 10, 2);
+        add_action('render_block_core/navigation-link', [$this, 'addIconsToBlockMenuItems'], 10, 2);
+    }
+
+    /**
+     * Adds icons to block menu items based on ACF field values.
+     */
+    public function addIconsToBlockMenuItems($blockContent, $block): string
+    {
+        if ('core/navigation-link' !== $block['blockName']) {
+            return $blockContent;
+        }
+
+        $menu = wp_get_nav_menu_object('mijn-zaken-sidebar');
+
+        if (! $menu) {
+            return $blockContent;
+        }
+
+        $menuItems = wp_get_nav_menu_items($menu->term_id);
+
+        if (! $menuItems) {
+            return $blockContent;
+        }
+
+        // Map menu items to URLs with their associated icons
+        $icons = [];
+        foreach ($menuItems as $item) {
+            $icon = get_field('menu_item_icon', $item);
+            if ($icon) {
+                $icons[$item->url] = esc_attr($icon);
+            }
+        }
+
+        // Check if the block URL matches a menu item URL and add the icon if it exists
+        $blockUrl = $block['attrs']['url'] ?? '';
+        if (isset($icons[$blockUrl])) {
+            $iconHtml = sprintf(
+                '<i class="wp-block-navigation-item__icon | fa-fw fa-regular fa-%s"></i> ',
+                esc_attr($icons[$blockUrl])
+            );
+
+            $blockContent = preg_replace(
+                '/(<span class="wp-block-navigation-item__label")/',
+                $iconHtml . '$1',
+                $blockContent
+            );
+        }
+
+        return $blockContent;
     }
 
     public function addIconsToMenuItems(array $items): array
     {
         foreach ($items as $item) {
-            $icon_name = get_post_meta($item->ID, 'menu_item_icon', true);
+            $iconName = get_post_meta($item->ID, 'menu_item_icon', true);
 
-            if (! empty($icon_name) && is_string($icon_name)) {
-                $icon_html = '<i class="fa-fw fa-regular fa-' . esc_attr($icon_name) . '"></i>';
-                $item->title = $icon_html . ' ' . $item->title;
+            if (! empty($iconName) && is_string($iconName)) {
+                $iconHTML = '<i class="fa-fw fa-regular fa-' . esc_attr($iconName) . '"></i>';
+                $item->title = $iconHTML . ' ' . $item->title;
             }
         }
 
