60ede9604a34ef9f47c7bb65756f37fdad10c6ba
Mike van den Hoek <mike@yard.nl>
2024-11-14 12:21:50 +0100

    (temp): disable eHerkenning login partial theme buren

--- htdocs/wp-content/themes/buren/config/home.php
+++ htdocs/wp-content/themes/buren/config/home.php
@@ -3,7 +3,7 @@
 return [
     'digid_is_active' => true,
     'digid_provider' => 'owc-gravityforms-digid',
-    'eherkenning_is_active' => true,
+    'eherkenning_is_active' => false,
     'eherkenning_provider' => 'owc-signicat-openid',
     'eidas_is_active' => true,
     'eidas_provider' => 'owc-signicat-openid',
