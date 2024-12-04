5e8309b192b43a46f00b2dad561fe1dda30b47bd
Mike van den Hoek <mike@yard.nl>
2024-12-04 16:26:52 +0100

    (feat): disable ssl verification for theme buren by hooking in to zaaksysteem plugin

--- htdocs/wp-content/themes/buren/functions.php
+++ htdocs/wp-content/themes/buren/functions.php
@@ -15,3 +15,9 @@ add_filter('owc_gravityforms_zaaksysteem_templates_to_validate', function ($temp
 add_filter('owc_gravityforms_digid_field_display_title', function () {
     return 'Klik hier om in te loggen';
 });
+
+/**
+ * Buren uses an ip-address instead of a domain in the 'zaaksysteem' configuration.
+ * This causes the SSL verification to fail, this filter disables it.
+ */
+add_filter('owc_gravityforms_zaaksysteem_disable_ssl_verification', '__return_true');
