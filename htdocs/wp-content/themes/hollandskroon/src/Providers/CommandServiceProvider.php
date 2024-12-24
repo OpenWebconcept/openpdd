af170a52087186dc5a0dfd830f2813e5372e38ba
Hugo Cox <h.n.c.cox@outlook.com>
2024-12-24 16:01:44 +0100

    (feat): add PronamicIdealCleanupCommand

---
+++ htdocs/wp-content/themes/hollandskroon/src/Providers/CommandServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace HK\Providers;
+
+use HK\Commands\PronamicIdealCleanupCommand;
+
+class CommandServiceProvider
+{
+	public function boot()
+	{
+		if (defined('WP_CLI') && WP_CLI) {
+			\WP_CLI::add_command('pronamic:cleanup', PronamicIdealCleanupCommand::class);
+		}
+	}
+}
