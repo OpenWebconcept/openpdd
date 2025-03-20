f3961602cb0d51fc09352345ae7a0de02b0c95f9
YvetteNikolov <yvettenikolov@gmail.com>
2025-03-20 13:38:31 +0100

    (feat): get something working

--- app/Sidebar/Sidebar.php
+++ app/Sidebar/Sidebar.php
@@ -38,22 +38,50 @@ class Sidebar
             return $blockContent;
         }
 
-        // Map menu items to URLs with their associated icons
+        // Map menu items to URLs with their associated icon details.
+        // If "menu_item_icon" has a value, use that.
+        // If empty, check "menu_item_muncipality_icon". If it's not "none",
+        // then mark it as a municipality icon.
         $icons = [];
         foreach ($menuItems as $item) {
-            $icon = get_field('menu_item_icon', $item);
-            if ($icon) {
-                $icons[$item->url] = esc_attr($icon);
+            $fieldIcon = get_field('menu_item_icon', $item);
+            if (! empty($fieldIcon)) {
+                $icons[$item->url] = [
+                    'type' => 'fontawesome',
+                    'icon' => esc_attr($fieldIcon),
+                ];
+            } else {
+                $municipalityIcon = get_field('menu_item_muncipality_icon', $item);
+                if (! empty($municipalityIcon) && 'none' !== $municipalityIcon) {
+                    $icons[$item->url] = [
+                        'type' => 'municipality',
+                        'icon' => esc_attr($municipalityIcon),
+                    ];
+                }
             }
         }
 
-        // Check if the block URL matches a menu item URL and add the icon if it exists
+        // Check if the block URL matches a menu item URL and add the icon if it exists.
         $blockUrl = $block['attrs']['url'] ?? '';
         if (isset($icons[$blockUrl])) {
-            $iconHtml = sprintf(
-                '<i class="wp-block-navigation-item__icon | fa-fw fa-regular fa-%s"></i> ',
-                esc_attr($icons[$blockUrl])
-            );
+            $iconData = $icons[$blockUrl];
+            if ('fontawesome' === $iconData['type']) {
+                $iconHtml = sprintf(
+                    '<i class="wp-block-navigation-item__icon | fa-fw fa-regular fa-%s"></i> ',
+                    $iconData['icon']
+                );
+            } elseif ('municipality' === $iconData['type']) {
+                // Construct the path to the municipality icon SVG file.
+                $svgPath = get_template_directory() . '/assets/img/municipality-icons/' . $iconData['icon'] . '.svg';
+                if (file_exists($svgPath)) {
+                    $iconHtml = file_get_contents($svgPath) . ' ';
+                } else {
+                    // Fallback in case file doesn't exist.
+                    $iconHtml = '';
+                }
+            } else {
+                $iconHtml = '';
+            }
 
             $blockContent = preg_replace(
                 '/(<span class="wp-block-navigation-item__label")/',
@@ -91,11 +119,11 @@ class Sidebar
             'fields' => [
                 [
                     'key' => 'field_66f549282efec',
-                    'label' => 'Font Awesome icoon naam',
+                    'label' => 'Font Awesome icoon',
                     'name' => 'menu_item_icon',
                     'aria-label' => '',
                     'type' => 'text',
-                    'instructions' => 'Bijv. "arrow-left"',
+                    'instructions' => 'Bijv. "arrow-left". Laat leeg om een gemeente icoon te gebruiken.',
                     'required' => 0,
                     'conditional_logic' => 0,
                     'wrapper' => [
@@ -109,6 +137,30 @@ class Sidebar
                     'placeholder' => '',
                     'prepend' => '',
                     'append' => '',
+                ],
+                [
+                    'key' => 'field_67dbede85dc28',
+                    'label' => 'Gemeente icoon',
+                    'name' => 'menu_item_muncipality_icon',
+                    'aria-label' => '',
+                    'type' => 'select',
+                    'instructions' => 'Bekijk de iconen op <a href="https://www.gemeenteniconen.nl/iconen" target="_blank">https://www.gemeenteniconen.nl/iconen</a>',
+                    'required' => 0,
+                    'conditional_logic' => 0,
+                    'wrapper' => [
+                        'width' => '',
+                        'class' => '',
+                        'id' => '',
+                    ],
+                    'choices' => $this->getMunicipalityIcons(),
+                    'default_value' => false,
+                    'return_format' => 'value',
+                    'multiple' => 0,
+                    'allow_null' => 0,
+                    'allow_in_bindings' => 0,
+                    'ui' => 1,
+                    'ajax' => 0,
+                    'placeholder' => '',
                 ],
             ],
             'location' => [
@@ -124,12 +176,33 @@ class Sidebar
             'position' => 'normal',
             'style' => 'default',
             'label_placement' => 'top',
-            'instruction_placement' => 'label',
+            'instruction_placement' => 'field',
             'hide_on_screen' => '',
             'active' => true,
             'description' => '',
             'show_in_rest' => 0,
-            'modified' => 1727360643,
         ]);
+    }
+
+    /**
+     * Returns an array of municipality icons.
+     */
+    public function getMunicipalityIcons(): array
+    {
+        $default = ['none' => 'Geen icoon geselecteerd'];
+        $directory = get_template_directory() . '/assets/img/municipality-icons/*.svg';
+        $iconPaths = glob($directory);
+
+        if (! is_array($iconPaths) || empty($iconPaths)) {
+            return $default;
+        }
+
+        $icons = [];
+        foreach ($iconPaths as $path) {
+            $icon = basename($path, '.svg');
+            $icons[$icon] = $icon;
+        }
+
+        return array_merge($default, $icons);
     }
 }
