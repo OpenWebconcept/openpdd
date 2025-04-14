cc8f3b79174264a79ccbfe53d0c4a04f63ee0bb0
Robert Bossaert <contact@robertbossaert.com>
2025-04-14 10:59:28 +0200

    (chore): remove bedrock dir if it already exists

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,6 +4,15 @@ set -e # exit if something goes wrong
 # Define the Bedrock directory
 BEDROCK_DIR="bedrock"
 
+# Remove the Bedrock directory if it already exists
+if [ -d "$BEDROCK_DIR" ]; then
+  echo "Removing existing Bedrock directory..."
+  rm -rf "$BEDROCK_DIR"
+fi
+
+# Create the Bedrock directory
+mkdir $BEDROCK_DIR
+
 # Define the environment variables
 DB_NAME="wordpress"
 DB_USER="wordpress"
@@ -12,11 +21,6 @@ DB_HOST="database"
 WP_ENV="development"
 WP_HOME="http://openpdd-new.lndo.site"
 WP_SITEURL="${WP_HOME}/wp"
-
-# Create the Bedrock directory if it doesn't exist
-if [ ! -d "$BEDROCK_DIR" ]; then
-  mkdir $BEDROCK_DIR
-fi
 
 # Navigate to the Bedrock directory
 cd $BEDROCK_DIR
@@ -39,13 +43,14 @@ composer config repositories.satispress composer https://packagist.yard.nl/satis
 composer config repositories.owc-gravityforms-digid vcs git@github.com:yardinternet/owc-gravityforms-digid.git
 composer config repositories.gravityforms-csp-fixer vcs git@github.com:yardinternet/plugin-gravityforms-csp-fixer.git
 composer config repositories.prefill-gravity-forms vcs git@github.com:OpenWebconcept/plugin-prefill-gravity-forms.git
-composer config repositories.owc-gravityforms-zaaksysteem vcs git@github.com:OpenWebconcept/plugin-owc-gravityforms-zaaksysteem.git
+composer config repositories.owc-mijn-services vcs git@github.com:OpenWebconcept/plugin-owc-mijn-services.git
+composer config repositories.owc-gravityforms-zgw vcs git@github.com:OpenWebconcept/plugin-owc-gravityforms-zgw.git
 composer config repositories.owc-openpdd-sanitizer vcs git@github.com:OpenWebconcept/plugin-owc-openpdd-sanitizer.git
 composer config repositories.idp-userdata vcs git@github.com:OpenWebconcept/idp-userdata.git
 composer config repositories.wp-packagist composer https://wpackagist.org
 
 # Require the plugins
-composer require plugin/owc-gravityforms-zaaksysteem plugin/owc-openpdd-sanitizer plugin/prefill-gravity-forms ypackagist/gravityforms wpackagist-plugin/cmb2
+composer require plugin/owc-mijn-services plugin/owc-gravityforms-zgw plugin/owc-openpdd-sanitizer plugin/prefill-gravity-forms ypackagist/gravityforms wpackagist-plugin/cmb2
 
 # Create theme (e.g. for Acorn support and pretty error reporting)
 cd web/app/themes && composer create-project roots/sage owc dev-main --ignore-platform-reqs && npm i && npm run build
