aa0561caf97fcd1847d7ba9f5c05ffa674d05ce2
Mike van den Hoek <mike@yard.nl>
2025-04-23 11:49:56 +0200

    (feat): prefill plugin with mapping options from theme dir

--- htdocs/wp-content/themes/buren/functions.php
+++ htdocs/wp-content/themes/buren/functions.php
@@ -21,3 +21,10 @@ add_filter('owc_gravityforms_digid_field_display_title', function () {
  * This causes the SSL verification to fail, this filter disables it.
  */
 add_filter('owc_gravityforms_zaaksysteem_disable_ssl_verification', '__return_true');
+
+/**
+ * Set the theme directory where the mapping option files are situated, for the OWC Prefill plugin.
+ */
+add_filter('owc_prefill_gravity_forms_theme_dir_mapping_options', function ($value) {
+    return __DIR__ . '/templates/owc-prefill/';
+}, 10, 1);
--- htdocs/wp-content/themes/noordwijk/functions.php
+++ htdocs/wp-content/themes/noordwijk/functions.php
@@ -5,3 +5,10 @@ declare(strict_types=1);
 defined('ABSPATH') || exit;
 
 add_filter('gform_enable_legacy_markup', '__return_true');
+
+/**
+ * Set the theme directory where the mapping option files are situated, for the OWC Prefill plugin.
+ */
+add_filter('owc_prefill_gravity_forms_theme_dir_mapping_options', function ($value) {
+    return __DIR__ . '/templates/owc-prefill/';
+}, 10, 1);
