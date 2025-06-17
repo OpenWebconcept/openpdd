ecbdb514fe833dde791cde794c18cf690b069e8d
YvetteNikolov <yvettenikolov@gmail.com>
2025-06-17 08:44:03 +0200

    (chore): temp style-dictionary

---
+++ style-dictionary.config.js
@@ -0,0 +1,10 @@
+// const {
+// 	createConfig,
+// } = require( '@nl-design-system-unstable/buren-design-tokens/style-dictionary-config' );
+// require( '@nl-design-system-unstable/buren-design-tokens/style-dictionary.config.js' );
+
+// module.exports = createConfig( {
+// 	backwardsCompatible: true,
+// 	selector: `.${ config.prefix }-theme`,
+// 	buildPath: 'src/styles/tokens/',
+// } );
