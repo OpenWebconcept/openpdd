1d93e76903c69dbb93212f97c57d405eeb62ad23
Mike van den Hoek <mike@yard.nl>
2025-09-29 14:33:35 +0200

    (feat): add templates to validate in ridderkerk theme

--- htdocs/wp-content/themes/barridderkerk/functions.php
+++ htdocs/wp-content/themes/barridderkerk/functions.php
@@ -4,4 +4,13 @@ declare(strict_types=1);
 
 defined('ABSPATH') || exit;
 
+add_filter('owc_gravityforms_zaaksysteem_templates_to_validate', function (array $templates) {
+    $templates[] = 'template-single-zaak';
+    $templates[] = 'template-mijn-zaken';
+    $templates[] = 'template-mijn-zaken-main';
+
+    return $templates;
+});
+
+
 add_filter('gform_enable_legacy_markup', '__return_true');
