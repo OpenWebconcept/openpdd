8508706beee73be8b7446da29a901b779bc5082e
Yvette Nikolov <48315669+YvetteNikolov@users.noreply.github.com>
2025-11-09 15:43:40 +0100

    feat: style digid and eherkenning gform fields the same (#100)

---
+++ htdocs/wp-content/themes/hoekschewaard/templates/page.php
@@ -0,0 +1,36 @@
+<?php declare(strict_types=1);
+get_header();
+
+?>
+<main class="page-main">
+	<div class="container">
+		<h2>Storing DigiD</h2>
+		<p>Er is momenteel een storing bij DigiD waardoor inloggen met DigiD niet mogelijk is. Onze excuses voor het ongemak. Probeer het later nogmaals.</p>
+	</div>
+    <div class="page-container | container bg-white p-3 p-md-4 p-lg-5 my-5 shadow">
+        <?php while (have_posts()) : the_post(); ?>
+        <header class="page-header | d-flex flex-column pb-3">
+            <h1 class="page-title | mb-3">
+                <?php echo get_the_title() ?>
+            </h1>
+			<?php if (get_current_blog_id() === env('GGD_SITE_ID', 5) || get_current_blog_id() === env('HW_SITE_ID', 4)): ?>
+				<div id="readspeaker_button1" class="rs_skip rsbtn rs_preserve mb-3">
+					<a rel="nofollow" class="rsbtn_play" accesskey="L"
+						title="Laat de tekst voorlezen met ReadSpeaker webReader"
+						href="//app-eu.readspeaker.com/cgi-bin/rsent?customerid=<?php echo get_current_blog_id() === env('GGD_SITE_ID', 5) ? '13499' : '8150'; ?>&amp;lang=nl_nl&amp;readid=readspeaker&amp;url=<?php echo get_permalink() ?>">
+						<span class="rsbtn_left rsimg rspart"><span class="rsbtn_text"><span>Lees
+									voor</span></span></span>
+						<span class="rsbtn_right rsimg rsplay rspart"></span>
+					</a>
+				</div>
+			<?php endif; ?>
+        </header>
+        <div class="page-content" id="readspeaker">
+            <?php the_content(); ?>
+        </div>
+
+        <?php endwhile; ?>
+    </div>
+</main>
+
+<?php get_footer();
